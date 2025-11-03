a72ca7cf293d46d407f5eda2ffe9246e6d1d3ad4
alvaro.vizuete <alvaro.vizuete@106-22.info2>
2025-11-03 10:27:15 +0100

    Ejercicios

--- Tema2/src/ejerciciosdowhile/Ejercicio05.java
+++ Tema2/src/ejerciciosdowhile/Ejercicio05.java
@@ -6,19 +6,25 @@ import java.util.Scanner;
 public class Ejercicio05 {
 
 	public static void main(String[] args) {
-		
+		// Creamos el Scanner
 		Scanner sc = new Scanner(System.in);
-		
+		// Creamos un random 
 		Random rd = new Random();
 		
+		// Declaramos la variable para el minimo
 		int min = 1;
-		int max = 1;
+		
+		// Declaramos la variable para el maximo
+		int max = 101;
+		
+		// Declaramos la variable para un nuúmero aleatorio
 		int numAleatorio;
 		
 		String resultado;
 		
 		
-		numAleatorio = rd.nextInt();
+		numAleatorio = rd.nextInt(min, max);
+		
 		
 		
 
